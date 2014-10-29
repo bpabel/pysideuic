8e4883eeee9654ca72a9c95862601d93dd83c9be
Brendan Abel <007brendan@gmail.com>
2014-10-28 17:01:08 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+import pysideuic
+
+
+setup(
+    name='pysideuic',
+    version=pysideuic.__version__,
+    license='MIT',
+    author='Brendan Abel',
+    packages=['pysideuic'],
+)
